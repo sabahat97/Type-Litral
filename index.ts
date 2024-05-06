fa42c525e92e517022af896d56a8f6467ff14f9a
syeda <sbtshakeel61@gmail.com>
2024-05-06 23:20:35 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+let food : "Chicken Burger" | "Biryani" | "Fried Rice Chicken Spicy" //type litral
+food = "Chicken Burger"
+console.log (food)
+
+food = "Biryani"
+console.log (food)
+
+food = "Fried Rice Chicken Spicy"
+console.log (food)
+
+food = "noodles"  //Error type
+//type "noodles" is not assignable to type "Chicken Burger" | "Biryani" | "Fried Rice Chicken Spicy"?
